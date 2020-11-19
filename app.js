a20776fe5b4be62435dedf3ef870b4bd8da6e562
LB <laurabradburn27@gmail.com>
2020-11-19 12:26:43 -0500

    Database Wikistack

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const layout = require('./views/layout')
+const { db } = require('./models');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'), {});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-	res.send('hello world');
+	res.send(layout(''));
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
 });
