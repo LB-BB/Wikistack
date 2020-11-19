dcb9632658a3fc78a4960fad491a822fb676d385
Allyson Camitta <allyson.camitta@gmail.com>
2020-11-19 16:36:23 -0500

    update addPage

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const addPage = require('../views/addPage');
 
-router.get("/", (req, res, next) => {
-    res.send('LOLO IS A VERY GOOD CAT')
-})
+router.get('/', (req, res, next) => {
+	res.redirect('/wiki');
+});
 //retrieve all wiki pages
 
-router.post("/", (req, res, next) => {
-    res.send('')
-})
+router.post('/', (req, res, next) => {
+	res.json(req.body);
+});
 //submit a new page to db
 
-router.get("/add", (req, res, next) => {
-    res.send('')
-})
+router.get('/add', (req, res, next) => {
+	res.send(addPage());
+});
 //get the add a page form
 
 module.exports = router;
