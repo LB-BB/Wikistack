cd144da4503a1261a56468fdbd018934406337ff
Allyson Camitta <allyson.camitta@gmail.com>
2020-11-19 12:59:47 -0500

    update routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const layout = require('./views/layout')
-const { db } = require('./models');
+const layout = require('./views/layout');
+const { db, Page, User } = require('./models');
+const sequelize = require('sequelize');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
@@ -12,13 +13,17 @@ app.get('/', (req, res) => {
 	res.send(layout(''));
 });
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  })
+db.authenticate().then(() => {
+	console.log('connected to the database');
+});
 
 const PORT = 3000;
+const init = async () => {
+	await Page.sync();
+	await User.sync();
+	app.listen(PORT, () => {
+		console.log(`App listening in port ${PORT}`);
+	});
+};
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+init();
